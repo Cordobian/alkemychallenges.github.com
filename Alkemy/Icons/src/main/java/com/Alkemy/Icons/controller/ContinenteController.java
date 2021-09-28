5996f7f9e7e106ed6b58a1fc7a3c92e2074c9507
Cordobes <cordobes.s.21@gmail.com>
2021-09-28 20:31:42 -0300

    commit 1
    Changes code

--- Alkemy/Icons/src/main/java/com/Alkemy/Icons/controller/ContinenteController.java
+++ Alkemy/Icons/src/main/java/com/Alkemy/Icons/controller/ContinenteController.java
@@ -1,6 +1,26 @@
 package com.Alkemy.Icons.controller;
 
-import javax.annotation.*;
+import com.Alkemy.Icons.entity.ContinenteEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("continente")
+@CrossOrigin("*")
 
 public class ContinenteController {
+
+    private ContienteService continenteService;
+
+    @Autowired
+    public  ContinenteController(ContienteService continenteService) { this.continenteService = continenteService;}
+
+    @GetMapping
+    public @ResponseBody
+    List<ContinenteEntity> getAll() {
+        List<ContinenteEntity> continentes = this.continenteService.getAllContinentes();
+        return continentes;
+    }
+
 }
---
+++ Alkemy/Icons/src/main/java/com/Alkemy/Icons/dto/ContinenteDTO.java
@@ -0,0 +1,11 @@
+package com.Alkemy.Icons.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ContinenteDTO {
+    private String imagen;
+    private String denominacion;
+}
